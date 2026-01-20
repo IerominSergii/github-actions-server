e254bd7d7dea4e2a0928ae50abc46a89fca00778
seromin <serhii.yeromin_nix@recordedfuture.com>
2026-01-20 20:14:03 +0200

    Refactor database connection logic and uncomment connection commands

--- data/database.js
+++ data/database.js
@@ -6,18 +6,19 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+// const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/??appName=Cluster0`;
 const client = new MongoClient(uri);
 
 console.log("Trying to connect to db");
 
 try {
-  // await client.connect();
-  // await client.db(dbName).command({ ping: 1 });
+  await client.connect();
+  await client.db(dbName).command({ ping: 1 });
   console.log("Connected successfully to server");
 } catch (error) {
   console.log("Connection failed.");
-  // await client.close();
+  await client.close();
   console.log("Connection closed.");
   process.exit(1);
 }
